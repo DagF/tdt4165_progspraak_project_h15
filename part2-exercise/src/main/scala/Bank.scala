18109ac21e9a80be74329e2fe13601ea767a632f
Dag Frode Solberg <tncditto@gmal.com>
2015-11-02 15:46:19 +0100

    started on exercise 2

--- part2-exercise/src/main/scala/Bank.scala
+++ part2-exercise/src/main/scala/Bank.scala
@@ -1,20 +1,35 @@
+import scala.concurrent.ExecutionContext
 import scala.concurrent.forkjoin.ForkJoinPool
 
 class Bank(val allowedAttempts: Integer = 3) {
 
-  private val uid = ???
+  class AccountID(var uid: Int) {}
+
+  private val uid = new AccountID(0)
   private val transactionsQueue: TransactionQueue = new TransactionQueue()
   private val processedTransactions: TransactionQueue = new TransactionQueue()
-  private val executorContext = ???
+  private val executorContext = ExecutionContext.fromExecutor(new ForkJoinPool(
+    
+  ))
 
   def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = {
     transactionsQueue push new Transaction(
       transactionsQueue, processedTransactions, from, to, amount, allowedAttempts)
   }
   
-  def generateAccountId: Int = ???
+  def generateAccountId: Int = {
+    var id =0
 
-  private def processTransactions: Unit = ???
+    uid synchronized{
+      id = uid.uid
+      uid.uid += 1
+    }
+    id
+  }
+
+  private def processTransactions: Unit = {
+    println("test")
+  }
 
   def addAccount(initialBalance: Double): Account = {
     new Account(this, initialBalance)
--- part2-exercise/src/main/scala/Transaction.scala
+++ part2-exercise/src/main/scala/Transaction.scala
@@ -7,21 +7,27 @@ object TransactionStatus extends Enumeration {
 }
 
 class TransactionQueue {
+
+  private var queue = List[Transaction]()
   
   // Remove and return the first element from the queue
-  def pop: Transaction = ???
+  def pop: Transaction = {
+    val tail = queue last;
+    queue = queue init;
+    tail
+  }
 
   // Return whether the queue is empty
-  def isEmpty: Boolean = ???
+  def isEmpty: Boolean = queue isEmpty
 
   // Add new element to the back of the queue
-  def push(t: Transaction): Unit = ???
+  def push(t: Transaction): Unit = queue = t :: queue
 
   // Return the first element from the queue without removing it
-  def peek: Transaction = ???
+  def peek: Transaction = queue head
 
   // Return an iterator to allow you to iterate over the queue
-  def iterator: Iterator[Transaction] = ???
+  def iterator: Iterator[Transaction] = queue.iterator
 }
 
 class Transaction(val transactionsQueue: TransactionQueue,
@@ -40,15 +46,18 @@ class Transaction(val transactionsQueue: TransactionQueue,
       to deposit amount
     }
     
-    if (from.uid < to.uid) from synchronized {
-      to synchronized {
-        doTransaction
-      }
-    } else to synchronized {
+    if (from.uid < to.uid)
       from synchronized {
-        doTransaction
+        to synchronized {
+          doTransaction
+        }
       }
-    }
+    else
+      to synchronized {
+        from synchronized {
+          doTransaction
+        }
+      }
     
     //Extend this method to satisfy new requirements.
 
